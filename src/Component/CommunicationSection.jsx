2ced3053baf3c9f8939a04d292cf14fe4538609b
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-01 17:04:13 +0300

    added icons for communication section

--- src/Component/CommunicationSection.jsx
+++ src/Component/CommunicationSection.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card from './Card';
 import PhoneCell from '../Icons/phone-cell.svg';
+import ChatIcon from '../Icons/chat-icon.svg';
+import VideoCallIcon from '../Icons/video-call-icon.svg';
 
 const CommunicationSection = ({ showSubtitle, subtitleText }) => (
   <div className="h-section flex flex-col justify-evenly px-sides">
-    <div className="">
+    <div className="mb-2">
       <h1 className="text-title pb-2 uppercase">we can communicate through</h1>
       {showSubtitle && <h3 className="text-subtitle ">{subtitleText}</h3>}
     </div>
+    <div className="flex justify-around"></div>
   </div>
 );
 
