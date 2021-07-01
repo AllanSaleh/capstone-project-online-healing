4cea6510cb03f4b4e636a9e7bc2e3d7050cada3b
Payam Abubakr Abdullah <Payam97abubakr@gmail.com>
2021-07-01 22:20:16 +0300

    added all the elements, needs design touchs and some tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
+import 'tailwindcss/tailwind.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   // This controls the direction of the page for RTL languages
   const { i18n } = useTranslation();
+
   useEffect(() => {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <div>App.js</div>;
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
