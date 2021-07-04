24bda99a349fb5c0ea52e3a8d7d687a1c8fe2e43
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-04 17:19:32 +0300

    created CreateAccount component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import CreateAccount from './Components/CreateAccount/CreateAccount';
 import './styles/main.css';
-
-import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -13,12 +12,7 @@ function App() {
 
   return (
     <div>
-      <NavBar />
-      <div className="text-center pt-navbar bg-blue-dark h-firstsection w-full px-sides">
-        First Section!
-      </div>
-      <div className="text-center bg-yellow-primary h-section w-full px-sides">Other Sections!</div>
-      <div className="text-center bg-blue-dark h-section w-full px-sides">Other Sections!</div>
+      <CreateAccount />
     </div>
   );
 }
---
+++ src/Components/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreateAccount = () => {
+  return (
+    <div className="px-sides">create account</div>
+  );
+  
+};
+
+export default CreateAccount;
