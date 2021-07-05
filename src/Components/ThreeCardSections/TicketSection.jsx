589c4d031c37a5fb2600c1f968fb251b6a62edb9
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-05 15:13:13 +0300

    added counselor section and refinded some style for buttons

--- src/Components/ThreeCardSections/TicketSection.jsx
+++ src/Components/ThreeCardSections/TicketSection.jsx
@@ -7,20 +7,20 @@ const TicketSection = () => (
     <h2 className="text-subtitle uppercase">
       purchase tickets that can be used to book appointments!
     </h2>
-    <div className="flex justify-around mt-24">
+    <div className="flex justify-around mt-28">
       <Card showIcon={false} title="5 Tickets" content="10$" showButton buttonText="Purchase" />
 
       <Card showIcon={false} title="25 Tickets" content="40$" showButton buttonText="Purchase" />
 
       <Card showIcon={false} title="50 Tickets" content="70$" showButton buttonText="Purchase" />
     </div>
-    <div className="flex flex-col justify-center items-center mt-32">
+    <div className="flex flex-col justify-center items-center mt-28">
       <h1 className="text-title uppercase">are you a counselor?</h1>
       <p className="text-paragraph w-2/3 text-center mt-8">
         Interested in joining our mental health platform? You decide your schedule and how much you
         want to work, we’ll take care of the client referrals and billing details!
       </p>
-      <button className="text-subtitle uppercase mt-8 bg-blue-dark rounded-lg py-2 px-4 border hover:bg-transparent hover:border-blue-dark hover:text-blue-dark transition-all duration-300">
+      <button className="text-subtitle uppercase my-8 bg-blue-dark rounded-lg py-2 px-4 border hover:bg-transparent hover:border-blue-dark hover:text-blue-dark transition-all duration-300">
         learn more
       </button>
     </div>
