3a72b715f2c0f37d80d0419e74b73992e4a3f2ef
Ahmed183 <ahmed1831990@gmail.com>
2021-07-07 15:06:58 +0300

    Refined CreditCard to work with SavedCards as well

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import './styles/main.css';
 
 import Purchase from './Components/PurchaseTicket/Purchase';
+import SavedCards from './Components/SavedCards/SavedCards';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -11,7 +12,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <Purchase />;
+  return <SavedCards />;
 }
 
 export default App;
