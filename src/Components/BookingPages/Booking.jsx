cd56354443f41e0858476e0d317d39d95c91c627
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-08 02:05:42 +0300

    created QuestionComponent

--- src/Components/BookingPages/Booking.jsx
+++ src/Components/BookingPages/Booking.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Booking = () => <div className="relative h-firstsection px-sides">booking</div>;
+const Booking = () => (<div className="relative h-firstsection px-sides">booking</div>);
 
 export default Booking;
---
+++ src/Components/BookingPages/QuestionComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const QuestionComponent = () => {
+    return ( 
+        <div>
+            QuestionComponent
+        </div>
+     );
+};
+export default QuestionComponent;
