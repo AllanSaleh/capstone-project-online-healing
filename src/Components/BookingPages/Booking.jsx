d651e671a1e71fed3e6c54f72f18a543d9d44876
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-08 02:07:14 +0300

    added title and  subtitle to Booking

--- src/Components/BookingPages/Booking.jsx
+++ src/Components/BookingPages/Booking.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Booking = () => (<div className="relative h-firstsection px-sides">booking</div>);
+const Booking = () => (
+  <div className="relative h-firstsection px-sides">
+    <h1 className="text-title uppercase mb-4"> let’s match you with the right therapist</h1>
+    <h3 className="text-subtitle opacity-50 text-justify">
+      Please fill out this short questionnaire to provide some general and anonymous background
+      about you and the issues you'd like to deal with in online therapy. It would help us match you
+      with the most suitable therapist for you.
+    </h3>
+  </div>
+);
 
 export default Booking;
