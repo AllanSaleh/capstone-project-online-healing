1c8c19429b729a5f81116e363f842a5bdd36b400
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-08 02:09:10 +0300

    added props to QuestionComponent and implemented

--- src/Components/BookingPages/QuestionComponent.jsx
+++ src/Components/BookingPages/QuestionComponent.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const QuestionComponent = () => {
-    return ( 
-        <div>
-            QuestionComponent
-        </div>
-     );
-};
+const QuestionComponent = ({ question, choices }) => (
+  <div>
+    {question && <h1 className="text-2xl">{question}</h1>}
+    {choices &&
+      choices.map((choice) => (
+        <option
+          className={`my-4 p-6 text-2xl mr-16 hover:cursor-pointer ${
+            choice.selected
+              ? 'bg-blue-dark text-white'
+              : 'bg-white border border-black border-opacity-20'
+          } rounded-md `}
+          value={choice.id}
+        >
+          {choice.content}
+        </option>
+      ))}
+  </div>
+);
 export default QuestionComponent;
