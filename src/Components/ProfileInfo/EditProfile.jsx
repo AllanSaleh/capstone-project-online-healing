5a643d9f0b352207ab33758a31a2b70929b18772
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-10 18:12:46 +0300

    added profile pic

--- src/Components/ProfileInfo/EditProfile.jsx
+++ src/Components/ProfileInfo/EditProfile.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import ProfilePic from '../../Icons/ProfilePic.svg';
 
 const EditProfile = () => (
   <div className="px-sides pt-navbar">
     <h3 className="text-subtitle text-red-500 w-full text-center">
       Please fill all the fields with correct and valid details to complete your profile.
     </h3>
+    <div className="flex">
+      <div className="w-1/3 p-4 mx-3 border border-black text-center">
+        <button
+          className="hover:cursor-pointer"
+          onClick={() => {
+            console.log('profile pic clicked');
+          }}
+        >
+          <img src={ProfilePic} alt="profile-pic" />
+        </button>
+      </div>
+      <div className="w-2/3 border border-black p-4 mx-3" />
+    </div>
   </div>
 );
 
