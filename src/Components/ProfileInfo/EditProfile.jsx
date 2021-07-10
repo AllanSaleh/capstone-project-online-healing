a07fecc6188c674cefa2e85c5139ce5936c0f170
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-11 02:43:39 +0300

    created custom input field for the profile component

--- src/Components/ProfileInfo/EditProfile.jsx
+++ src/Components/ProfileInfo/EditProfile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfilePic from '../../Icons/ProfilePic.svg';
+import EditProfileSection from './EditProfileSection';
 
 const EditProfile = () => (
   <div className="px-sides pt-navbar">
@@ -7,7 +8,7 @@ const EditProfile = () => (
       Please fill all the fields with correct and valid details to complete your profile.
     </h3>
     <div className="flex">
-      <div className="w-1/3 p-4 mx-3 border border-black text-center">
+      <div className="w-2/5 p-4 mx-2 text-center">
         <button
           className="hover:cursor-pointer"
           onClick={() => {
@@ -17,7 +18,9 @@ const EditProfile = () => (
           <img src={ProfilePic} alt="profile-pic" />
         </button>
       </div>
-      <div className="w-2/3 border border-black p-4 mx-3" />
+      <div className="w-3/5 p-4 mx-2">
+        <EditProfileSection title="profile info" />
+      </div>
     </div>
   </div>
 );
---
+++ src/Components/ProfileInfo/EditProfileSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileInputField from './ProfileInputField';
+import BirthDateIcon from '../../Icons/BirthDateIcon.svg';
+
+const EditProfileSection = ({ title, fileds }) => (
+  <div>
+    <h1 className="text-title uppercase">{title}</h1>
+    <ProfileInputField title= 'full name' type="text" id="full-name" icon={BirthDateIcon}/>
+  </div>
+);
+
+export default EditProfileSection;
