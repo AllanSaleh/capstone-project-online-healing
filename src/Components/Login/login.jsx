ccb27a31568c0dd82919a26d6acb69b760d47c1c
Payam Abubakr Abdullah <Payam97abubakr@gmail.com>
2021-07-12 22:58:24 +0300

    made icons work as buttons, fixed the input borders, increased padding of the input fields

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import HP_illustration from './HP_illustration.svg';
-import Loginwith from './loginwith.svg';
+import Facebook from './Facebook.svg';
+import Google from './Google.svg'
+import Or from './or.svg'
+
 function Login(){
     return(
             
@@ -9,10 +12,10 @@ function Login(){
                 <div className="text-5xl pb-16">
                  LOGIN
                 </div>
-                    <div className=" w-96 h-72 ring-1 bg-white shadow-xl rounded-lg ">
+                    <div className=" w-96 h-72 bg-white shadow-xl rounded-lg ">
                         <div className="flex flex-col">
-                            <input type="email" id="email" placeholder="email" className="rounded-sm ring-1 pl-2 ml-4 mt-10 mr-5 h-12" />
-                            <input type="password" name="password" id="password" placeholder="password" className="rounded-sm ring-1 p-2 ml-4 mt-5 mr-5 h-12" />
+                            <input type="email" id="email" placeholder="email" className="border border-black border-opacity-40 rounded-sm pl-4 ml-4 mt-10 mr-5 h-12" />
+                            <input type="password" name="password" id="password" placeholder="password" className="border border-black border-opacity-40 rounded-sm pl-4 ml-4 mt-5 mr-5 h-12" />
                             <div className="flex flex-row justify-between" >
                             <button className="mt-10 ml-4  pt-2 pb-2 pl-10 pr-10 text-subtitle rounded-md bg-blue-dark border hover:bg-white hover:text-blue-dark hover:border-blue-dark">LOG IN</button>
                             <button className="mt-10 mr-5 border pt-2 pb-2 pl-10 pr-10 text-subtitle rounded-md border-blue-dark border-opacity-100 bg-white text-blue-dark hover:bg-blue-dark hover:text-black hover:border-blue-dark">SIGN UP</button>
@@ -20,7 +23,11 @@ function Login(){
                         </div>
                     </div>
                     <div className="mt-10">
-                        <img src={Loginwith} alt="loginwith"  />
+                        <img src={Or} alt="Or"  />
+                        <div className="flex items-center justify-evenly px-16 mt-4">
+                        <button><img src={Facebook} alt="facebook" /></button>
+                        <button><img src={Google} alt="google" /> </button>
+                        </div>
                     </div>
                 </div>
                 <div>
