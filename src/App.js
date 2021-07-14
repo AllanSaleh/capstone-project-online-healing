61b0bf25ed7930e29351eac4e655c153a3ef818d
Ahmed183 <ahmed1831990@gmail.com>
2021-07-14 20:07:10 +0300

    Commiting snoor's latest update since she has technical problems

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import CreateAccount from './Components/CreateAccount/CreateAccount';
 import './styles/main.css';
-import Working from './Components/WorkingWithHealling/Working';
-import Description from './Components/Description/Description';
-import LandingPage from './Components/landing page/landingpage';
+
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
-import AddNewCard from './Components/AddNewCard/AddNewCard';
-import ThreeCardSection from './Components/ThreeCardSections/ThreeCardSection';
-import threeCardSectionData from './mockdata/data';
+import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -21,7 +16,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Working />
+      <WorkingWithHealing />
       <Footer />
     </div>
   );
