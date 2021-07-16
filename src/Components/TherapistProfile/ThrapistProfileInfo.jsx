eecb02b672359cece0ff8d4b0286f42df93beced
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-16 18:11:12 +0300

    added Profile pic to TherapistProfile

--- src/Components/TherapistProfile/ThrapistProfileInfo.jsx
+++ src/Components/TherapistProfile/ThrapistProfileInfo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ProfilePic from '../../Icons/ProfilePic.svg';
 
 const TherapistProfileInfo = () => (
-  <div>
-      TherapistProfileInfo
+  <div className="flex px-sides pt-navbar">
+    <div className="w-2/5 p-4 mx-2 text-center">
+      <button className="hover:cursor-pointer">
+        <img src={ProfilePic} alt="profile-pic" />
+      </button>
+    </div>
   </div>
 );
 
