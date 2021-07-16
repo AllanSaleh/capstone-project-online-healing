532933a7272ee55cce5007ded54698bbeffaf686
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-16 18:23:53 +0300

    added fileds to the therapist profile

--- src/Components/TherapistProfile/ThrapistProfileInfo.jsx
+++ src/Components/TherapistProfile/ThrapistProfileInfo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ProfilePic from '../../Icons/ProfilePic.svg';
+import ProfileInputField from '../ProfileInfo/ProfileInputField';
+import BirthDateIcon from '../../Icons/BirthDateIcon.svg';
 
 const TherapistProfileInfo = () => (
   <div className="flex px-sides pt-navbar">
@@ -7,6 +9,20 @@ const TherapistProfileInfo = () => (
       <button className="hover:cursor-pointer">
         <img src={ProfilePic} alt="profile-pic" />
       </button>
+    </div>
+    <div className="w-3/5 p-4 mx-2">
+    <h1 className="text-title uppercase">therapist profile</h1>
+      <ProfileInputField title="full name" type="text" id="full-name" />
+      <div className="flex my-6">
+        <label htmlFor="bio" className="text-subtitle capitalize inline-block w-1/3">
+          bio
+        </label>
+        <div className="w-2/3 h-64 inline-flex items-center border border-black border-opacity-20 rounded-lg pl-4">
+          <textarea id="bio" name="bio" className="w-11/12 h-full outline-none py-3 resize-none" />
+        </div>
+      </div>
+      <ProfileInputField title="birth date" type="text" id="birth-date" icon={BirthDateIcon} />
+      <ProfileInputField title="phone number" type="text" id="phone-number" />
     </div>
   </div>
 );
