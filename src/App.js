9e1613872d1fdfb795ab3073d8faa65641a64095
snoor-madih <59646350+snoor-madih@users.noreply.github.com>
2021-07-16 11:09:05 +0300

    working with healing (#24)
    * working with healing
    
    * working with healing2
    
    * working with healing 3
    
    * Commiting snoor's latest update since she has technical problems
    
    Co-authored-by: Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
    Co-authored-by: Ahmed183 <ahmed1831990@gmail.com>
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import CreateAccount from './Components/CreateAccount/CreateAccount';
 import './styles/main.css';
-import Description from './Components/Description/Description';
-import LandingPage from './Components/landing page/landingpage';
+import Working from './Components/WorkingWithHealling/Working';
+
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
-import AddNewCard from './Components/AddNewCard/AddNewCard';
-import ThreeCardSection from './Components/ThreeCardSections/ThreeCardSection';
-import threeCardSectionData from './mockdata/data';
+import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -19,20 +16,8 @@ function App() {
 
   return (
     <div>
-      <CreateAccount />
       <NavBar />
-      <LandingPage />
-      <Description />
-      <AddNewCard />
-      {threeCardSectionData.map((section) => (
-        <ThreeCardSection
-          title={section.title}
-          subtitle={section.subtitle}
-          cardsData={section.cardsData}
-          isCounselor={section.isCounselor}
-        />
-      ))}
-      <div className="text-center bg-blue-dark h-section w-full px-sides">Other Sections!</div>
+      <WorkingWithHealing />
       <Footer />
     </div>
   );
