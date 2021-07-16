75d5853b3c90d1ef0e952b181c77fcbd1638a45e
Ahmed183 <ahmed1831990@gmail.com>
2021-07-16 19:09:07 +0300

    Reviewed the whole code with Avraz and the compoenent is done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LandingPage from './Components/landing page/landingpage';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
+import ContactPage from './Components/Contact Page/ContactPage';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -16,13 +17,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return (
-    <div>
-      <NavBar />
-      <FirstSection />
-      <Footer />
-    </div>
-  );
+  return <ContactPage />;
 }
 
 export default App;
