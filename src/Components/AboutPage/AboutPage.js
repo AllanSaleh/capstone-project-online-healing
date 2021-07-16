0c74211cc437c78b8924f3a12eb9d7a06be67acc
Ahmed183 <ahmed1831990@gmail.com>
2021-07-16 23:20:32 +0300

    Reviewed and updated the whole thing

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import NavBar from "../NavBar/NavBar"
-import Footer from "../Footer/Footer"
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
 import Founding from './Founding';
 import Healing from './Healing';
 
 export default function AboutPage() {
   return (
-    <div>
+    <div className="h-firstsection">
       <NavBar />
       <Healing />
       <Founding />
