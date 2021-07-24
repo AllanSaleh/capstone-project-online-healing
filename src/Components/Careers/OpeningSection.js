55a8cf06fabeec866ff8e6cdeb6b5a4e3364e994
Ahmed183 <ahmed1831990@gmail.com>
2021-07-24 15:57:20 +0300

    Done!

--- src/Components/Careers/OpeningSection.js
+++ src/Components/Careers/OpeningSection.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Position from './Position';
 
 export default function OpeningSection() {
-  const [positions, setPositions] = useState([
+  const positions = [
     {
       title1: 'Senior Software Developer | Backend | Remote in the USA',
       subtitle1:
@@ -38,18 +38,18 @@ export default function OpeningSection() {
       title2: 'Engineering',
       subtitle2: 'Application Development',
     },
-  ]);
+  ];
 
   return (
     <div className="h-section flex flex-col justify-around px-sides">
       <div>
-        <div className="text-title">CURRENT OPEN POSITIONS</div>
-        <div className="text-subtitle text-gray-700">
+        <div className="text-3xl lg:text-title">CURRENT OPEN POSITIONS</div>
+        <div className="text-xl lg:text-subtitle text-gray-700">
           Please send us an email with the application title as the subject with
           an attached CV in PDF format at:
         </div>
         <a
-          className="text-subtitle text-blue-dark hover:underline"
+          className="text-xl lg:text-subtitle text-blue-dark hover:underline"
           href="mailto:recruitment@healing.com"
         >
           recruitment@healing.com
