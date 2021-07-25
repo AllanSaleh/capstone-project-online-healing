b9c6f00acc623e0cfcd9a20d20fdb90b9c378767
Payam0057 <39165734+pamoo@users.noreply.github.com>
2021-07-25 12:14:50 +0300

    the login component is done (#36)
    * the login component is done
    
    * login button fixed
    
    * removed the extra div
    
    * made icons work as buttons, fixed the input borders, increased padding of the input fields
    
    * deleted the unused image
    
    * Reviewed and Updated LoginPage
    
    Co-authored-by: Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
    Co-authored-by: Ahmed183 <ahmed1831990@gmail.com>
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Working from './Components/WorkingWithHealling/Working';
 import FirstSection from './Components/Contact Page/FirstSection';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
+import AddNewCard from './Components/AddNewCard/AddNewCard';
+import Login from './Components/Login/login';
+import OpeningSection from './Components/Careers/OpeningSection';
 import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
 import ContactPage from './Components/Contact Page/ContactPage';
 
@@ -19,7 +22,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <Purchase />;
+  return <Login />;
 }
 
 export default App;
