43ea0265285101ef62010cab2d29126514570db0
avrazebary <40139953+avrazebary@users.noreply.github.com>
2021-07-25 11:34:35 +0300

    created first section of contact page (#17)
    * created first section of contact page
    
    * fixed Radios
    
    * Reviewed the whole code with Avraz and the compoenent is done
    
    Co-authored-by: allan <allansaleh95@gmail.com>
    Co-authored-by: Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
    Co-authored-by: Ahmed183 <ahmed1831990@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
-import Working from './Components/WorkingWithHealling/Working';
 
+import FirstSection from './Components/Contact Page/FirstSection';
+import Description from './Components/Description/Description';
+import LandingPage from './Components/landing page/landingpage';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
-import AddNewCard from './Components/AddNewCard/AddNewCard';
-import OpeningSection from './Components/Careers/OpeningSection';
 import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
+import ContactPage from './Components/Contact Page/ContactPage';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -16,15 +17,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return (
-    <div>
-      <NavBar />
-      <div className="text-center bg-blue-dark h-firstsection w-full px-sides">First Section!</div>
-      <OpeningSection />
-      <WorkingWithHealing />
-      <Footer />
-    </div>
-  );
+  return <ContactPage />;
 }
 
 export default App;
