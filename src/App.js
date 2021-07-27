faf1152feec3a90a8b846b309eb777461e64f46a
Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
2021-07-27 05:38:59 +0300

    Saved Cards Page Done (#37)
    * Moved some files to keep folder structure consistent
    
    * Finished the openings page
    
    * Fine tuned the margins a bit
    
    * Almost died trying to make the scrollable cards holder
    
    * Improved the whole thing by a ton!
    
    * Visuals Done, Trying to implement card selection process
    
    * Made CreditCard universal and added JS
    
    * Finished
    
    * Refined CreditCard to work with SavedCards as well

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
 
-import Purchase from './Components/PurchaseTicket/Purchase';
-import Description from './Components/Description/Description';
-import LandingPage from './Components/landing page/landingpage';
-import Working from './Components/WorkingWithHealling/Working';
-import FirstSection from './Components/Contact Page/FirstSection';
-import NavBar from './Components/NavBar/NavBar';
-import Footer from './Components/Footer/Footer';
-import AddNewCard from './Components/AddNewCard/AddNewCard';
-import Login from './Components/Login/login';
-import OpeningSection from './Components/Careers/OpeningSection';
-import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
-import ContactPage from './Components/Contact Page/ContactPage';
+import SavedCards from './Components/SavedCards/SavedCards';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -22,7 +11,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <Login />;
+  return <SavedCards />;
 }
 
 export default App;
