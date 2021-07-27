968b6a4d5525036b8296cbeeec8e740d329ab590
Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
2021-07-27 08:35:20 +0300

    Created the ThankYou component to be used universally (#42)
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
 
-import SavedCards from './Components/SavedCards/SavedCards';
+import NavBar from './Components/NavBar/NavBar';
+import Footer from './Components/Footer/Footer';
+import ThankYou from './Components/ThankYou/ThankYou';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -11,7 +13,15 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <SavedCards />;
+  return (
+    <div>
+      <NavBar />
+      <ThankYou
+        subtitle={`Your Sign Up request has been received, you will soon receive a confirmation email. \nPlease follow the steps in the email to complete and activate your account.`}
+      />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
