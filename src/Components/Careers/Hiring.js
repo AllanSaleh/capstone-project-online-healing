19b6a300223ca839f05ed20019e921daf49498da
Ahmed183 <ahmed1831990@gmail.com>
2021-07-27 14:12:46 +0300

    Improved sizing and added functionality to the button

--- src/Components/Careers/Hiring.js
+++ src/Components/Careers/Hiring.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Hiring() {
   return (
-    <div className="bg-yellow-primary px-sides h-1/4 flex flex-col justify-evenly">
+    <div className="bg-yellow-primary px-sides h-2/5 flex flex-col justify-evenly">
       <div className="text-title">OUR HIRING PHILOSOPHY</div>
-      <div className="text-subtitle text-gray-700">
+      <div className="text-subtitle text-gray-600">
         To build the very best SEO tools, we need a workforce that reflects the diversity of our
         communities and customers. We want Online Therapy to be a place where everyone feels welcome
         and engaged, bar none. It's our mission and promise to interview a diverse and
