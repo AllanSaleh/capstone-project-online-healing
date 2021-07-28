08db1ff762bfd5ff2f4a833ace90a3cfff3764f0
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-07-28 23:03:05 +0300

    patched some responsiveness of ticket section and added "Are you a counselor?" card to ticket section

--- src/Components/Home/TicketSection.jsx
+++ src/Components/Home/TicketSection.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Card from './Card';
 
 const TicketSection = () => (
-  <div className=" md:h-section flex flex-col justify-evenly px-sides">
-    <div>
+  <div className="flex flex-col justify-evenly px-sides">
+    <div className="pb-16 pt-8">
       <h1 className="text-xl md:text-4xl xl:text-title uppercase">
         Purchase tickets
       </h1>
@@ -35,6 +35,28 @@ const TicketSection = () => (
         showButton
         buttonText="Purchase"
       />
+    </div>
+    <div
+      className="flex flex-col justify-around my-16 px-4 lg:px-8 rounded-lg"
+      style={{ boxShadow: '0px 4px 12px 4px rgba(178, 171, 171, 0.25)' }}
+    >
+      <h1 className="text-2xl md:text-4xl lg:text-title uppercase mx-auto py-4">
+        are you a counselor?
+      </h1>
+      <p className="text-sm md:text-md lg:text-subtitle text-justify md:text-center text-opacity-50 my-4 ">
+        Interested in joining our mental health platform? You decide your
+        schedule and how much you want to work, we’ll take care of the client
+        referrals and billing details!
+      </p>
+
+      <div className="text-center my-8">
+        <button
+          type="button"
+          className="bg-blue-dark py-2 px-4 uppercase text-paragraph border text-black hover:bg-transparent hover:border-blue-dark hover:text-blue-dark  transition-all duration-200 rounded-md"
+        >
+          learn more
+        </button>
+      </div>
     </div>
   </div>
 );
