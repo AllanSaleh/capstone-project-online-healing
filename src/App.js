5c206a9eeb31b1f1ccdc4436720ecaea14779eed
Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
2021-07-29 14:42:50 +0300

    Responsive Blog & Recommended (#90)
    * Made the BlogPage folder and the BlogBody
    
    * Patched snoor's Recommended into BlogPage
    
    * BlogPage is now responsive
    
    Co-authored-by: Allan <allansaleh95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
 
-import ThankYou from './Components/ThankYou/ThankYou';
+import BlogPage from './Components/Blogs/BlogPage';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import AddNewCard from './Components/AddNewCard/AddNewCard';
@@ -15,15 +15,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return (
-    <div>
-      <NavBar />
-      <ThankYou
-        subtitle={`Your request has been received, you'll soon receive a confirmation email. \nPlease follow the steps in the email to complete and activate your account.`}
-      />
-      <Footer />
-    </div>
-  );
+  return <BlogPage />;
 }
 
 export default App;
