b561d677ae29142828176506c8c324537c0deeb3
snoor-madih <59646350+snoor-madih@users.noreply.github.com>
2021-07-29 06:57:07 -0700

    working with healing is responsive now (#92)
    * working with healling is responsive now
    
    * working with healling is responsive enhancement
    
    Co-authored-by: Ahmed183M <80580251+Ahmed183M@users.noreply.github.com>
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
 
-import RecentBlogs from './Components/Home/RecentBlogs';
+import WorkingWithHealing from './Components/WorkingWithHealing/WorkingWithHealing';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import AddNewCard from './Components/AddNewCard/AddNewCard';
@@ -15,14 +15,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return (
-    <div>
-      <NavBar />
-      <div className="text-center bg-blue-dark h-firstsection w-full px-sides">First Section!</div>
-      <RecentBlogs />
-      <Footer />
-    </div>
-  );
+  return <WorkingWithHealing />;
 }
 
 export default App;
