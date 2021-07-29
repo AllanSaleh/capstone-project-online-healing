e0f24f1d15c05e6eddff793b7bc6d1c0b7729e29
Ahmed183 <ahmed1831990@gmail.com>
2021-07-29 20:16:39 +0300

    Added new updates to change

--- src/Components/1Change/AboutPage/AboutPage.jsx
+++ src/Components/1Change/AboutPage/AboutPage.jsx
@@ -5,10 +5,10 @@ import Payam from './Images/Payam.svg';
 export default function AboutPage() {
   return (
     <div className="h-firstsection">
-      <div className="h-1/2 px-sides pt-navbar">
-        <div className="text-title mt-8">HEALING</div>
-        <div className="text-subtitle text-gray-700 mb-8">A Cool One Liner!</div>
-        <div className="text-paragraph">
+      <div className="h-1/2 px-sides pt-navbar md:text-sm lg:text-lg md:h-1/2 md:mb-10 sm:text-sm	mb-20">
+        <div className="sm:text-title mt-8 text-sm 	">HEALING</div>
+        <div className="sm:text-subtitle  text-gray-700 mb-8  text-sm mt-5	">A Cool One Liner!</div>
+        <div className="sm:text-paragraph  text-sm leading-snug 	">
           At Healing, wedd believe there is a better way to do things. A more valuable way where
           customers are earned rather than bought. We're obsessively passionate about it, and our
           mission is to help people achieve it. We focus on search engine optimization. It's one of
@@ -17,11 +17,11 @@ export default function AboutPage() {
           education, and community.
         </div>
       </div>
-      <div className="h-1/2 flex justify-around items-center bg-blue-light px-sides">
-        <img className="w-1/3" src={Payam} alt="Payam" />
-        <div className="w-1/2">
-          <div className="text-title">Our Founding</div>
-          <div className="text-subtitle">
+      <div className="sm:h-1/2 h-3/4 flex justify-around  items-center bg-blue-light px-sides relative py-56 sm:flex-row flex-col  ">
+        <img className="w-full sm:w-1/3" src={Payam} alt="Payam" />
+        <div className="w-full sm:1/2 sm:mb-10 sm:mt-10	 md:bg-blue-light sm:ml-10  ">
+          <div className=" sm:text-title text-sm 	my-8	">Our Founding</div>
+          <div className="  sm:text-subtitle text-sm leading-snug	">
             Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started
             as a blog and an online community where some of the world's therapists shared their
             research and ideas. We launched the Beginner's Guide to Therapy and our first study, and
