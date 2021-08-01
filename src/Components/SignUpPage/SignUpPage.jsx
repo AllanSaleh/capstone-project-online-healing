69edd5ede6f7bdc8413a5d9845012793f20c46e7
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-08-01 12:44:41 +0300

    handled input fields change methods

--- src/Components/SignUpPage/SignUpPage.jsx
+++ src/Components/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuidv4';
 
 import SignupImage from './Images/SignupImage.svg';
 import Facebook from './Images/Facebook.svg';
@@ -13,6 +14,20 @@ export default function SignUpPage() {
   const [users, setUsers] = useState([]);
   const usersRef = firebase.firestore().collection('users');
 
+  const signupInfo = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    birthdate: 0,
+  };
+
+  const signupConfirmation = {
+    confirm_email: '',
+    confirm_password: '',
+  };
+
+  // gets all users from firebase
   const getUsers = () => {
     usersRef.onSnapshot((querySnapshot) => {
       const items = [];
@@ -23,59 +38,82 @@ export default function SignUpPage() {
     });
   };
 
+  const handleChange = (event) => {
+    signupInfo[event.target.id] = event.target.value;
+  };
+
+  const handleChangeConfirmation = (event) => {
+    signupConfirmation[event.target.id] = event.target.value;
+  };
+
+  const handleSignup = (event) => {
+    console.log('signed up');
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
-
   return (
-    <div className=" flex flex-col lg:flex lg:flex-row items-center justify-evenly max-h-full lg:-mt-20 mt-20 lg:h-firstsection">
+    <div className=" flex flex-col lg:flex lg:flex-row items-center justify-evenly max-h-full lg:h-firstsection mt-navbar lg:m-0">
       <div className="lg:w-auto w-48">
         <img src={SignupImage} alt="SignupImage" />
       </div>
-      <div className="lg:w-auto transform translate-y-9 w-72 lg:transform lg:translate-y-0">
-        <div className="lg:text-5xl text-3xl lg:text-left text-center lg:pb-16 ">SIGN UP NOW</div>
+      <div className="lg:w-auto w-72">
+        <div className="lg:text-5xl text-3xl lg:text-left text-center lg:pb-16 ">
+          SIGN UP NOW
+        </div>
 
         <div className="lg:h-3/5  h-auto flex flex-col justify-evenly shadow-xl rounded-lg p-4 ">
           <div className="lg:w-full flex flex-col lg:flex lg:flex-row lg:justify-between">
             <input
               type="text"
-              id="first-name"
+              id="first_name"
+              name="first_name"
               placeholder="First Name"
               className="rounded-lg ring-1 h-12 p-2 lg:w-52 lg:mr-4 mt-4"
+              onChange={handleChange}
             />
             <input
               type="text"
-              id="last-name"
+              id="last_name"
+              name="last_name"
               placeholder="Last Name"
               className="rounded-lg ring-1 h-12 p-2 lg:w-52 mt-4"
+              onChange={handleChange}
             />
           </div>
           <input
-            type="Email"
-            name="Email"
+            type="email"
+            name="email"
             id="email"
             placeholder="Email"
             className="rounded-lg ring-1 h-12 p-2 mt-4"
+            onChange={handleChange}
           />
           <input
-            type="Confirm Email"
-            name="Confirm Email"
-            id="Confirm-email"
+            type="email"
+            name="confirm_email"
+            id="confirm_email"
             placeholder="Confirm Email"
             className="rounded-lg ring-1 h-12 p-2 mt-4"
+            onChange={handleChangeConfirmation}
           />
           <div className="w-full lg:flex flex flex-col lg:flex-row justify-between">
             <input
               type="password"
+              name="password"
               id="password"
               placeholder="Password"
               className="rounded-lg ring-1 h-12 p-2 lg:w-52 lg:mr-4 mt-4"
+              onChange={handleChange}
             />
             <input
               type="password"
-              id="confirm-password"
+              name="confirm_password"
+              id="confirm_password"
               placeholder="Confirm Password"
               className="rounded-lg ring-1 h-12 p-2 lg:w-52 mt-4"
+              onChange={handleChangeConfirmation}
             />
           </div>
           <div className="flex flex-col lg:flex lg:flex-row lg:items-center justify-evenly w-full mt-6">
@@ -105,14 +143,16 @@ export default function SignUpPage() {
             <Link to="/Login">
               <button
                 type="button"
-                className="lg:w-40 w-auto h-12 text-subtitle rounded-lg border-2 text-blue-dark border-blue-dark hover:bg-blue-dark hover:text-black hover:border-transparent mt-4"
+                className="lg:w-40 w-auto h-12 text-subtitle rounded-lg border-2 text-blue-dark border-blue-dark hover:bg-blue-dark hover:text-black hover:border-transparent mt-4 transition-all duration-300"
               >
                 LOG IN
               </button>
             </Link>
             <button
               type="button"
-              className="shadow-2xl lg:w-40 w-auto h-12 text-subtitle bg-blue-dark rounded-lg border-2 border-transparent hover:bg-white hover:text-blue-dark hover:border-blue-dark mt-4"
+              className="shadow-xl lg:w-40 w-auto h-12 text-subtitle bg-blue-dark rounded-lg border-2 border-transparent hover:bg-white hover:text-blue-dark hover:border-blue-dark mt-4
+              transition-all duration-300"
+              onClick={handleSignup}
             >
               SIGN UP
             </button>
