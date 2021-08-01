09ad8a7db6ea34714ec47fb960c61b84f5f5230d
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-08-01 18:12:02 +0300

    patched responsiveness bugs for EditProfile component

--- src/Components/EditProfilePage/ProfileInputField.jsx
+++ src/Components/EditProfilePage/ProfileInputField.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 const ProfileInputField = ({ title, id, type, icon }) => (
   <div className="flex items-center my-6">
-    <label htmlFor={id} className="text-subtitle capitalize inline-block w-1/3">
+    <label
+      htmlFor={id}
+      className="text-sm md:text-lg lg:text-subtitle capitalize inline-block w-1/3"
+    >
       {title}
     </label>
     <div className="w-2/3 inline-flex items-center border border-black border-opacity-20 rounded-lg pl-4">
-      <input type={type} id={id} name={id} className="w-11/12 h-full outline-none py-3" />
+      <input
+        type={type}
+        id={id}
+        name={id}
+        className="w-11/12 h-full outline-none py-3"
+      />
       {icon && (
-        <button type="button">
-          <img src={icon} alt="profileIcon" />
+        <button className="w-5 h-5 mr-2">
+          <img src={icon} alt="icon" />
         </button>
       )}
     </div>
